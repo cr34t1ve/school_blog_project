7cd0c500f54df69dafc8ff8359f69d41221de3f9
Master Sofua Desmond <mrsofua@gmail.com>
2021-09-19 14:51:28 +0000

    CategoryPost isolated

--- lib/screens/HomePage/homepage.dart
+++ lib/screens/HomePage/homepage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:school_blog_project/components/BlogPost.dart';
+import 'package:school_blog_project/components/CategoryPost.dart';
 import 'package:school_blog_project/components/VideoItem.dart';
 
 class HomePage extends StatefulWidget {
@@ -74,61 +75,11 @@ class _HomePageState extends State<HomePage>
                 ],
               ),
             ),
-            Expanded(
-              child: ListView(
-                children: [
-                  SizedBox(height: 20.0),
-                  Container(
-                    height: 200.0,
-                    child: ListView.separated(
-                        shrinkWrap: true,
-                        scrollDirection: Axis.horizontal,
-                        itemCount: 3,
-                        separatorBuilder: (context, index) {
-                          return SizedBox(
-                            width: 10.0,
-                          );
-                        },
-                        itemBuilder: (context, index) {
-                          return VideoItem();
-                        }),
-                  ),
-                  SizedBox(
-                    height: 20.0,
-                  ),
-                  Column(
-                    children: [
-                      BlogPost(),
-                      SizedBox(
-                        height: 10.0,
-                      ),
-                      BlogPost(),
-                      SizedBox(
-                        height: 10.0,
-                      ),
-                      BlogPost(),
-                      SizedBox(
-                        height: 10.0,
-                      ),
-                      // ListView.separated(
-                      //     shrinkWrap: true,
-                      //     itemCount: 3,
-                      //     separatorBuilder: (context, index) {
-                      //       return SizedBox(
-                      //         height: 10.0,
-                      //       );
-                      //     },
-                      //     itemBuilder: (context, index) {
-                      //       return BlogPost();
-                      //     }),
-                    ],
-                  )
-                ],
-              ),
-            )
+            CategoryPage()
           ],
         ),
       ),
     );
   }
 }
+
