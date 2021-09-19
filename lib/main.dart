508cf095ae5ff4da3131fc49a859f034e5f6276a
Master Sofua Desmond <mrsofua@gmail.com>
2021-09-19 15:29:46 +0000

    'SavedPosts' initialised

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:school_blog_project/screens/BlogPostDetails/BlogPostDetails.dart';
 import 'package:school_blog_project/screens/HomePage/homepage.dart';
+import 'package:school_blog_project/screens/SavedPosts/SavedPosts.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: BlogPostDetails(),
+    home: SavedPosts(),
     theme: ThemeData(
         appBarTheme: AppBarTheme(backgroundColor: Colors.white, elevation: 0.0),
         fontFamily: 'Manrope',
---
+++ lib/screens/SavedPosts/SavedPosts.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class SavedPosts extends StatefulWidget {
+  const SavedPosts({Key? key}) : super(key: key);
+
+  @override
+  _SavedPostsState createState() => _SavedPostsState();
+}
+
+class _SavedPostsState extends State<SavedPosts> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        top: true,
+        child: Column(),
+      ),
+    );
+  }
+}
